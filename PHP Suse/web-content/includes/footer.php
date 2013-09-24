691be3c034e19c160b335bd0d1c3a50368a23921
Oliver Kinne <oliver_kinne@hotmail.com>
2013-09-24 21:01:33 +0100

    Added IPv6 logo

--- PHP Suse/web-content/includes/footer.php
+++ PHP Suse/web-content/includes/footer.php
@@ -1,15 +1,19 @@
 	<footer>
 		<section>
-			<p><span class="button tel"><span></span>Phone</span> <strong>Call</strong><br />
-				M: <a class="tel" href="tel:+447773278142">07773 278 142</a></p>
+			<div>
+				<p><span class="button tel"><span></span>Phone</span> <strong>Call</strong><br />
+					M: <a class="tel" href="tel:+447773278142">07773 278 142</a></p>
+	
+				<p><span class="button email"><span></span>Email</span> <strong>Email</strong><br />
+					<a href="mailto:me@oliverkinne.com">me@oliverkinne.com</a></p>
+	
+				<p class="social"><span class="button social"><span></span>Social</span> <strong>Follow Me</strong><br />
+					<a class="linkedin" href="http://uk.linkedin.com/in/oliverkinne"><span></span>LinkedIn</a> <a class="github" href="http://github.com/oliverkinne"><span></span>GitHub</a></p>
+	
+				<p class="ipv6"><a href="http://www.worldipv6launch.org/"><img src="/img/ipv6.png" width="45" height="52" alt="IPv6 World Launch Day"></a></p>
 
-			<p><span class="button email"><span></span>Email</span> <strong>Email</strong><br />
-				<a href="mailto:me@oliverkinne.com">me@oliverkinne.com</a></p>
-
-			<p class="social"><span class="button social"><span></span>Social</span> <strong>Follow Me</strong><br />
-				<a class="linkedin" href="http://uk.linkedin.com/in/oliverkinne"><span></span>LinkedIn</a> <a class="github" href="http://github.com/oliverkinne"><span></span>GitHub</a></p>
-
-			<div class="clear">&nbsp;</div>
+				<div class="clear">&nbsp;</div>
+			</div>
 		</section>
 	</footer>
 </body>
