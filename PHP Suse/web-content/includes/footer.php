c1ce6667b076a4d3362ef1107b13a825ea5d219c
Oliver Kinne <oliver_kinne@hotmail.com>
2013-09-25 22:30:45 +0100

    Made site device agnostic and used two methods of creating rounded corners
    I used media queries to make the site automatically adjust to different
    screen/window sizes. I also employed two methods of creating circular
    areas. One using an round image as a background image, with a pre-IE9
    fix for scaling the image - the other using simply the border-radius
    style.

--- PHP Suse/web-content/includes/footer.php
+++ PHP Suse/web-content/includes/footer.php
@@ -1,10 +1,10 @@
 	<footer>
 		<section>
 			<div>
-				<p><span class="button tel"><span></span>Phone</span> <strong>Call</strong><br />
+				<p class="tel"><span class="button tel"><span></span>Phone</span> <strong>Call</strong><br />
 					M: <a class="tel" href="tel:+447773278142">07773 278 142</a></p>
 	
-				<p><span class="button email"><span></span>Email</span> <strong>Email</strong><br />
+				<p class="email"><span class="button email"><span></span>Email</span> <strong>Email</strong><br />
 					<a href="mailto:me@oliverkinne.com">me@oliverkinne.com</a></p>
 	
 				<p class="social"><span class="button social"><span></span>Social</span> <strong>Follow Me</strong><br />
