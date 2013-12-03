03d18520ccd75ebe81b155e245dec480eabeb17a
Oliver Kinne <oliver_kinne@hotmail.com>
2013-12-03 19:31:03 +0000

    Added function to calculate date range, especially for "to present" ranges

--- PHP Suse/web-content/projects/index.php
+++ PHP Suse/web-content/projects/index.php
@@ -5,7 +5,7 @@
 
 			<aside>
 				<h4>Clans of Macaria</h4>
-				<p class="dates">October 2013 - Present</p>
+				<p class="dates"><?php print_date_range('1 October 2013', null) ?></p>
 				<p class="team"></p>
 				<p>Clans of Macaria is an on-line, turn-based game for multiple players. The game system uses elements from many popular board games, but as a whole is completely original.</p>
 				<p>It is still in development and is programmed in PHP, using a MySQL database to store the game states and related data. The site uses JQuery, CSS and HTML.</p>
@@ -32,7 +32,7 @@ GitHubEvents('http://www.gmodules.com/ig/proxy?url=https://api.github.com/repos/
 
 			<aside>
 				<h4>My Resume Website</h4>
-				<p class="dates">September 2013 - Present</p>
+				<p class="dates"><?php print_date_range('1 September 2013', null) ?></p>
 				<p class="team"></p>
 				<p>The oliverkine.com website was built in PHP, with SEO and pageload performance in mind. CloudFlare CDN was applied to make the site even more efficient and load even more quickly.</p>
 				<p>CSS is used on the homepage to create rollover effects, as well as for styling.</p>
@@ -59,7 +59,7 @@ GitHubEvents('http://www.gmodules.com/ig/proxy?url=https://api.github.com/repos/
 
 			<aside>
 				<h4>LSi Online Website Rebuild</h4>
-				<p class="dates">June 2012 - April 2013 (11 months)</p>
+				<p class="dates"><?php print_date_range('1 June 2012', '30 April 2013') ?></p>
 				<p class="team">Team Members: <a href="http://uk.linkedin.com/pub/alex-dato/60/267/835/">Alex Dato</a>, <a href="http://uk.linkedin.com/pub/lee-baldock/1/b94/5a0/">Lee Baldock</a></p>
 				<p>The LSi Online website was rebuilt to make it device agnostic, allowing it to display equally well on desktop and laptop computers, as well as tablets and smartphones. The site was also built to load quickly, using &quot;lazyload&quot; for images, CSS sprites, late loading of JavaScript, caching of searches and other database driven information on the server and pre-building of database driven information where possible.</p>
 				<p>The site also employs SEO techniques, uses metadata for page classification and content tagging (OpenGraph) and is built with visually impaired users in mind as well.</p>
@@ -82,7 +82,7 @@ GitHubEvents('http://www.gmodules.com/ig/proxy?url=https://api.github.com/repos/
 
 			<aside>
 				<h4>Online Manuals System</h4>
-				<p class="dates">January 2012 - Present (over 1 year)</p>
+				<p class="dates"><?php print_date_range('1 January 2012', null) ?></p>
 				<p class="team"></p>
 				<p>I developed an online manuals system for exhibitions and events. The system allows exhibitors to submit their orders to exhibition contractors, as well as complete regulatory and other required documentation to help the event management process.</p>
 				<p>The system is highly customizable to allow seamless integration with an exhibition's branding and design. It can cope with virtually any type, quantity and complexity of forms, including multi-step forms, required fields, fields that become required based on other selections, enforcement of field types (date, number, etc.), secure fields (credit card details, etc.) and file uploads (attachments, etc.).</p>
@@ -107,7 +107,7 @@ Budgets</p>
 
 			<aside>
 				<h4>PLASA Lib - JQuery and JScript</h4>
-				<p class="dates">January 2011 - Present (over 2 years)</p>
+				<p class="dates"><?php print_date_range('1 January 2011', null) ?></p>
 				<p class="team"></p>
 				<p>I built a library of JScript and JQuery functionality for rapid website development. The library is not based on existing code, but was programmed from the ground up. It covers string functions, date and number formatting functions, character conversion functions for UTF-8 and other HTML character sets, as well as server- and client-side form validation routines.</p>
 			</aside>
@@ -127,7 +127,7 @@ Budgets</p>
 
 			<aside>
 				<h4>SagePay Credit Card Payment Processing Module</h4>
-				<p class="dates">January 2012 - Present (over 1 year)</p>
+				<p class="dates"><?php print_date_range('1 January 2012', null) ?></p>
 				<p class="team"></p>
 				<p>I developed a payment processing module, using the direct integration method of SagePay, to allow easy credit card processing on all of PLASA's websites.</p>
 			</aside>
@@ -147,7 +147,7 @@ Budgets</p>
 
 			<aside>
 				<h4>Searchable Book Database OTree</h4>
-				<p class="dates">September 1998 to October 1998 (2 months)</p>
+				<p class="dates"><?php print_date_range('1 September 1998', '31 October 1998') ?></p>
 				<p class="team"></p>
 				<p>Development of bespoke, searchable books database on CD, which contained full information about tens of thousands of books, including title, author, description, ISBN, binding, pages, illustrations, etc. as well as a scanned cover image. I developed a new database system, called OTree, which was based on BTree, to create a highly compressed database of the books that would fit onto CD and would allow very fast searches and information retrieval.</p>
 			</aside>
@@ -263,5 +263,38 @@ function strShorten($description, $length) {
 		$description = preg_replace('/ [^ ]*$/', ' ', substr($description, 0, $length)) . '...';
 
 	return $description;
+}
+
+function print_date_range($date1, $date2) {
+	$date1 = strtotime($date1);
+	echo date('F Y', $date1) . ' - ';
+
+	if ($date2 == null) {
+		$date2 = strtotime(date('d F Y'));
+		echo ' Present';
+	}
+	else {
+		$date2 = strtotime($date2);
+		echo date('F Y', $date2);
+	}
+
+	$diff = abs($date2 - $date1);
+	$years = floor($diff / (365*60*60*24));
+	$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+
+	echo ' (';
+	if ($years > 0) {
+		echo $years . ' year';
+		if ($years > 1)
+			echo 's';
+	}
+	if ($years > 0 && $months > 0)
+		echo ' ';
+	if ($months > 0) {
+		echo $months . ' month';
+		if ($months > 1)
+			echo 's';
+	}
+	echo ')';
 }
 ?>
