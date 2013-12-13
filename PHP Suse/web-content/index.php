1c7bc9ec6be393553b491e04ebe6150b79649439
Oliver Kinne <oliver_kinne@hotmail.com>
2013-12-13 18:26:27 +0000

    Changed emphasis to employed work - no programming

--- PHP Suse/web-content/index.php
+++ PHP Suse/web-content/index.php
@@ -13,9 +13,9 @@
 			<aside class="home">
 				<p>I am a developer at heart and relish exploring new technologies and learning new skills, applying them to business processes to improve efficiency and increase productivity.</p>
 				<p>I have many years' experience of software and web development, with a thorough understanding of databases, servers, networking and related technologies. I deliver high quality work and have successfully managed numerous projects of varying size for a number of clients.</p>
-				<p>I am happy to work on a freelance basis and have good
+				<p>I am happy to work on an employed basis, so if you need a highly self-motivated member of staff with a positive attitude to work in your team, or if you want to create a new team and need someone with the necessary IT skills and experience who can manage a group of like-minded people, I would relish the opportunity of an interview.</p>
+				<p>I am also happy to work on a freelance basis and have good
 working relationships with designers, copywriters, photographers and print production experts. So if you are looking to re-design your website or create a brand new web presence, if you need to update your promotial materials or want to update your brand, or if you would like advice on setting up your first office, improving your existing IT systems, moving to cloud services, tuning your databases or updating your networking infrastructure, I can help.</p>
-				<p>I am also happy to work on an employed basis, so if you need a highly self-motivated member of staff with a positive attitude to work in your team, or if you want to create a new team and need someone with the necessary IT skills and experience who can manage a group of like-minded people, I would relish the opportunity of an interview.</p>
 			</aside>
 
 			<div class="clear">&nbsp;</div>
