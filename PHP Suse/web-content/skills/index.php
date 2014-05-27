52d8d7fd20377d6f82190fe1189f7b427fb670a4
Oliver Kinne <me@oliverkinne.com>
2014-05-27 18:25:21 +0100

    Updated content to reflect new skills and change the emphasis

--- PHP Suse/web-content/skills/index.php
+++ PHP Suse/web-content/skills/index.php
@@ -5,11 +5,11 @@
 
 			<aside>
 				<h4>Summary</h4>
-				<p>Management: I have successfully managed a large number of projects of varying sizes in terms of budget, size of team and duration.</p>
+				<p>Management: I have successfully managed a large number of projects of varying sizes in terms of budget, size of team and duration. I also have recently achieved AgilePM Foundation certification.</p>
+				<p>Cloud Services: I have migrated in-house Exchange infrastructure to Microsoft Office 365 and implemented Amazon Web Services' S3 to extend file storage, as well as set up AWS EC2 to build a LAMP test server.</p>
+				<p>CMS/Wikis: I have developed a number of systems that allow comany employees to update website content as necessary, without the need for an IT person. I have hands-on experience with Joomla and WordPress, and installed, configured, administered and developed plug-ins and templates for both. I have also set up and maintained a Confluence server to maintain a Wiki.</p>
 				<p>Web Development: I have built a number of website of varying sizes and complexities, which are viewable on desktop computers, laptops, tablets and smartphones alike. All websites have been built to aim for high search engine ranking, and to make them accessible to people with impairments. I have hands-on knowledge of HTML, CSS, JQuery, JavaScript, JScript, VBScript and PHP. I have applied load performance improvements, such as lazy image and late script loading, and several caching techniques to reduce page load time and improve server performance. I have also built a function library to help rapid web development.</p>
 				<p>HTML Emails: I have hands-on experience with MailChimp and created many emails that are compatible with all common clients and designed to increase the effectiveness of the marketing campaign. I have built HTML emails from scratch, without the use of software tools or HTML email builders.</p>
-				<p>CMS/Wikis: I have developed a number of systems that allow comany employees to update website content as necessary, without the need for an IT person. I have hands-on experience with Joomla and installed, configured and administered Confluence, a popular collaboration system and Wiki.</p>
-				<p>Cloud Services: I have migrated in-house Exchange infrastructure to Microsoft Office 365 and implemented Amazon Web Services' S3 to extend file storage, as well as set up AWS EC2 to build a LAMP test server.</p>
 				<p>Databases: I have many years' working knowledge of many database platforms, from MS Access to SQL Server and MySQL. I have designed and built a large number of databases from scratch, including defining indexes and keys for high throughput, fast searching, or a balance of both.</p>
 				<p>Software Development: I have many years' experience of software development in a number of programming languages, including OOP. I also contribute to a number of public GitHub projects.</p>
 				<p>Software Packages: I have hands-on knowledge of the whole Adobe Creative Suite, in particular Dreamweaver, Flash, Photoshop and Illustrator, as well as the Microsoft Office suite, such as Access, Excel and Word. I use GitHub to maintain a number of in-house projects and in my spare time contribute to public projects.</p>
@@ -18,6 +18,17 @@
 			<aside class="side">
 				<h5>Keywords</h5>
 				<ul>
+					<li>AgilePM</li>
+					<li>Cloud Computing</li>
+					<li>Virtualization</li>
+					<li>Office 365</li>
+					<li>Amazon Web Services</li>
+					<li>CDN</li>
+					<li>Github</li>
+					<li>Joomla</li>
+					<li>WordPress</li>
+					<li>Confluence</li>
+					<li>Project Management</li>
 					<li>JScript</li>
 					<li>JavaScript</li>
 					<li>JQuery</li>
@@ -26,24 +37,16 @@
 					<li>CSS</li>
 					<li>PHP</li>
 					<li>ASP</li>
-					<li>CDN</li>
 					<li>VBScript</li>
 					<li>SQL Server</li>
 					<li>MySQL</li>
 					<li>MS Access</li>
 					<li>Dreamweaver</li>
 					<li>Web Development</li>
-					<li>Joomla</li>
-					<li>Confluence</li>
 					<li>Email Marketing</li>
 					<li>CRM</li>
-					<li>Github</li>
 					<li>Photoshop</li>
 					<li>Flash</li>
-					<li>Cloud Computing</li>
-					<li>Virtualization</li>
-					<li>Office 365</li>
-					<li>Amazon Web Services</li>
 					<li>YaST</li>
 					<li>IIS</li>
 					<li>Network Administration</li>
@@ -52,7 +55,6 @@
 					<li>Team Leadership</li>
 					<li>IT Strategy</li>
 					<li>Operating Systems</li>
-					<li>Project Management</li>
 					<li>System Administration</li>
 					<li>Active Directory</li>
 					<li>Microsoft Exchange</li>
