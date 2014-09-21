f426dfc73f1fe3c3c9b14714e0ed2607fcef9477
Oliver Kinne <me@oliverkinne.com>
2014-09-21 21:07:56 +0100

    Added page specific titles and descriptions.

--- PHP Suse/web-content/includes/header.php
+++ PHP Suse/web-content/includes/header.php
@@ -16,9 +16,42 @@ if ($server != 'me.oliverkinne.com' && $server != 'www.oliverkinne.com' && $serv
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
 	<meta name="keywords" content="Oliver Kinne, websites, domain names, networking, cloud services, hardware, software, virtualisation, IT consultancy, support" />
-	<meta name="description" content="Oliver Kinne - Web Development / IT Consultancy" />
+	<meta name="description" content="<?php
+switch ($path) {
+	case "/skills/":
+?>I have over two decades' experience in IT, covering software and web development, email marketing, CMS, CRM, cloud services, databases, servers, networking, security and many related technologies. I deliver high quality work and have successfully managed numerous projects of varying size for a number of clients.<?php
+		break;
+	case "/projects/":
+?>I have successfully managed a large number of projects of varying sizes in terms of budget, size of team and duration. I also have recently achieved AgilePM Foundation certification.<?php
+		break;
+	case "/cv/":
+?>During my long career in IT, I have supported multiple offices in different time zones, with several dozen staff, including home workers. I have also administered, maintained and consolidated multiple, business critical servers.<?php
+		break;
+	case "/contact/":
+?>Please contact me for further information.<?php
+		break;
+	default:
+?>Oliver Kinne - Web Development / IT Consultancy<?php
+		break;
+}
+?>" />
 
-	<title>Oliver Kinne - Web Development / IT Consultancy</title>  
+	<title><?php
+switch ($path) {
+	case "/skills/":
+?>Skills - <?php
+		break;
+	case "/projects/":
+?>Projects - <?php
+		break;
+	case "/cv/":
+?>CV - <?php
+		break;
+	case "/contact/":
+?>Contact - <?php
+		break;
+}
+?>Oliver Kinne - Web Development / IT Consultancy</title>  
 
 	<meta name="HandheldFriendly" content="True">
 	<meta name="MobileOptimized" content="320">
