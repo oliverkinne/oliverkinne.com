61d73f7a55c4adc33db89bbb83c31b67128d4a92
Oliver Kinne <me@oliverkinne.com>
2014-09-29 21:21:07 +0100

    Fixed external 303 redirects

--- html/includes/footer.php
+++ html/includes/footer.php
@@ -8,7 +8,7 @@
 					<a href="mailto:me@oliverkinne.com">me@oliverkinne.com</a></p>
 	
 				<p class="social"><span class="button social"><span></span>Social</span> <strong>Follow Me</strong><br />
-					<a class="linkedin" href="http://uk.linkedin.com/in/oliverkinne"><span></span>LinkedIn</a> <a class="spiceworks" href="http://community.spiceworks.com/people/oliverkinne/"><span></span>Spiceworks</a> <a class="github" href="http://github.com/oliverkinne"><span></span>GitHub</a></p>
+					<a class="linkedin" href="http://uk.linkedin.com/in/oliverkinne"><span></span>LinkedIn</a> <a class="spiceworks" href="http://community.spiceworks.com/people/oliverkinne/"><span></span>Spiceworks</a> <a class="github" href="https://github.com/oliverkinne"><span></span>GitHub</a></p>
 	
 				<p class="ipv6"><a href="http://www.worldipv6launch.org/"><img src="/img/ipv6.png" width="45" height="52" alt="IPv6 World Launch Day"></a></p>
 
